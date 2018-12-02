b939ce93c1b02630c408f2bd5091dff28596a60a
Mike Basta <mikebasta@gmail.com>
2018-12-02 13:36:17 -0800

    Removed data validation as it will be handled by the admin form

--- src/utils/statsCalc.js
+++ src/utils/statsCalc.js
@@ -3,7 +3,7 @@
 function mergeStats(exisitingData, enteredData){
     //creating new array with map method, which adds or updates data with element.(item)
  const updatedData = enteredData.map(element => {
-     const hits = getHits(element["1b"], element["2b"], element["3b"], element.hr);
+     const hits = getHits(Number(element["1b"]), Number(element["2b"]), Number(element["3b"]), Number(element.hr));
      element.hits = hits;
      const atBats = getAtBats(hits, element["o"])
      element.ab = atBats;
@@ -12,36 +12,37 @@ function mergeStats(exisitingData, enteredData){
  });
  return updatedData;
 }
+//data type validation. Only number inputs are valid
+/*function isNumber(arr){
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof (arr[i])!== 'number') {
+            return false; 
+        }
+    }
+   return true;
+}*/
 //Derive hits from user entered data to use in other functions
 function getHits(singles, doubles, triples, homeRuns){
-    if (
-        typeof singles !== 'number'||
-        typeof doubles !== 'number'||
-        typeof triples !== 'number'||
-        typeof homeRuns !== 'number') {
-            return Error('Inputs for getHits need to be integers');
-        }
+    /*if (!isNumber([singles, doubles, triples, homeRuns])) {
+        return Error('Inputs for getHits need to be integers');
+    }*/
    return singles + doubles + triples + homeRuns;
-  }
+}
   
 
 function getAtBats(hits, outs){
-    if (typeof hits !== 'number'|| typeof outs !== 'number') {
+    /*if (!isNumber([hits, outs])) {
         return Error('Inputs for getAtBats need to be integers');
-    }
+    }*/
     return hits + outs;
 }
 
 function getTotalBases(singles, doubles, triples, homeRuns){
-    if (
-        typeof singles !== 'number'||
-        typeof doubles !== 'number'||
-        typeof triples !== 'number'||
-        typeof homeRuns !== 'number') {
-            return Error('Inputs for getTotalBases need to be integers');
-        }
-   return singles + (doubles * 2) + (triples * 3) + (homeRuns * 4);
-  }
+    /*if (!isNumber([singles, doubles, triples, homeRuns])) {
+        return Error('Inputs for getTotalBases need to be integers');
+    }*/
+    return singles + (doubles * 2) + (triples * 3) + (homeRuns * 4);
+}
 
 function getRunsCreated(hits, walks, caughtStealing, totalBases, stolenBases, atBats){
     return ((hits + walks - caughtStealing) * (totalBases + (stolenBases * .55)) / (atBats + walks));
