086bdc5e70c7b25c292174fa63862af37465a7c4
Mike Basta <mikebasta@gmail.com>
2018-12-02 14:39:20 -0800

    mergeStats function updated to add atBats, totalBases, and runsCreated to player object

--- src/utils/statsCalc.js
+++ src/utils/statsCalc.js
@@ -4,9 +4,13 @@ function mergeStats(exisitingData, enteredData){
     //creating new array with map method, which adds or updates data with element.(item)
  const updatedData = enteredData.map(element => {
      const hits = getHits(Number(element["1b"]), Number(element["2b"]), Number(element["3b"]), Number(element.hr));
-     element.hits = hits;
-     const atBats = getAtBats(hits, element["o"])
+     element.h = hits;
+     const atBats = getAtBats(hits, Number(element["o"]));
      element.ab = atBats;
+     const totalBases = getTotalBases(Number(element["1b"]), Number(element["2b"]), Number(element["3b"]), Number(element.hr));
+     element.tb = totalBases;
+     const runsCreated = getRunsCreated(hits, Number(element["bb"]), Number(element["cs"]), totalBases, Number(element["sb"]), atBats);
+     element.rc = runsCreated;
      return element;
 
  });
@@ -23,20 +27,20 @@ function mergeStats(exisitingData, enteredData){
 }*/
 //Derive hits from user entered data to use in other functions
 function getHits(singles, doubles, triples, homeRuns){
-   return singles + doubles + triples + homeRuns;
+   return (singles + doubles + triples + homeRuns).toString();
 }
   
 
 function getAtBats(hits, outs){
-    return hits + outs;
+    return (hits + outs).toString();
 }
 
 function getTotalBases(singles, doubles, triples, homeRuns){
-    return singles + (doubles * 2) + (triples * 3) + (homeRuns * 4);
+    return (singles + (doubles * 2) + (triples * 3) + (homeRuns * 4)).toString();
 }
 
 function getRunsCreated(hits, walks, caughtStealing, totalBases, stolenBases, atBats){
-    return ((hits + walks - caughtStealing) * (totalBases + (stolenBases * .55)) / (atBats + walks));
+    return (((hits + walks - caughtStealing) * (totalBases + (stolenBases * .55)) / (atBats + walks))).toString();
 }
 
 
@@ -56,4 +60,4 @@ const test = [{
  cs: '',
  ab: '',
 }];
-console.log('test', mergeStats([], test)[0].hits);
+console.log('test', mergeStats([], test)[0].h);
