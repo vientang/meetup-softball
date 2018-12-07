9f8a6cc70139509404c11f2d3ded2b202ccd4f61
Mike Basta <mikebasta@gmail.com>
2018-12-07 08:35:22 -0800

    fixed type converstion so functions calculate stats correctly, then convert to string before placed in player object

--- src/utils/statsCalc.js
+++ src/utils/statsCalc.js
@@ -2,26 +2,25 @@
 //and creates new array that includes new calculated stats.
 function mergeStats(exisitingData, enteredData){
     //creating new array with map method, which adds or updates data with element.(item)
-    //goal is to calculate game stats and update player object
  const updatedData = enteredData.map(element => {
      const hits = getHits(Number(element["1b"]), Number(element["2b"]), Number(element["3b"]), Number(element.hr));
-     element.h = hits;
+     element.h = hits.toString();
      const atBats = getAtBats(hits, Number(element.o));
-     element.ab = atBats;
+     element.ab = atBats.toString();
      const totalBases = getTotalBases(Number(element["1b"]), Number(element["2b"]), Number(element["3b"]), Number(element.hr));
-     element.tb = totalBases;
+     element.tb = totalBases.toString();
      const runsCreated = getRunsCreated(hits, Number(element.bb), Number(element.cs), totalBases, Number(element.sb), atBats);
-     element.rc = runsCreated;
+     element.rc = runsCreated.toString();
      const average = getAverage(hits, atBats);
-     element.avg = average;
+     element.avg = average.toString();
      const onBasePercentage = getonBasePercentage(hits, Number(element.bb), atBats, Number(element.sac));
-     element.obp = onBasePercentage;
+     element.obp = onBasePercentage.toString();
      const slugging = getSlugging(totalBases, atBats);
-     element.slg = slugging;
+     element.slg = slugging.toString();
      const onBasePlusSlugging = getOPS(onBasePercentage, slugging);
-     element.ops = onBasePlusSlugging;
+     element.ops = onBasePlusSlugging.toString();
      const weightedOnBaseAverage = getWOBA(Number(element.bb), Number(element["1b"]), Number(element["2b"]), Number(element["3b"]), Number(element.hr), atBats, Number(element.sac));
-     element.woba = weightedOnBaseAverage;
+     element.woba = weightedOnBaseAverage.toString();
      return element;
 
  });
@@ -37,58 +36,61 @@ function mergeStats(exisitingData, enteredData){
    return true;
 }*/
 //Derive hits from user entered data to use in other functions
+
 function getHits(singles, doubles, triples, homeRuns){
-   return (singles + doubles + triples + homeRuns).toString();
+   return (singles + doubles + triples + homeRuns);
 }
-  
+
 
 function getAtBats(hits, outs){
-    //TODO: modify to prevent string concatenation in testing. results are showing 1 + 1 = 11 instead of 1 + 1 = 2
-    return (hits + outs).toString();
+    return (hits + outs);
 }
 
 function getTotalBases(singles, doubles, triples, homeRuns){
-    return (singles + (doubles * 2) + (triples * 3) + (homeRuns * 4)).toString();
+    return (singles + (doubles * 2) + (triples * 3) + (homeRuns * 4));
 }
 
 function getRunsCreated(hits, walks, caughtStealing, totalBases, stolenBases, atBats){
-    return (((hits + walks - caughtStealing) * (totalBases + (stolenBases * .55)) / (atBats + walks))).toString();
+    return (((hits + walks - caughtStealing) * (totalBases + (stolenBases * .55)) / (atBats + walks)));
 }
 
 function getAverage(hits, atBats){
-    return (hits / atBats).toString();
+    return (hits / atBats);
 }
 
 function getonBasePercentage(hits, walks, atBats, sacrifices){
-    return ((hits + walks) / (atBats + walks + sacrifices)).toString();
+    return ((hits + walks) / (atBats + walks + sacrifices));
 }
 
 function getSlugging(totalBases, atBats){
-    return (totalBases / atBats).toString();
+    return (totalBases / atBats);
 }
 
 function getOPS(onBase, slugging){
-    return (onBase + slugging).toString();
+    return (onBase + slugging);
 }
 
 function getWOBA(walks, singles, doubles, triples, homeRuns, atBats, sacrifices){
-    return ((.69 * walks + .888 * singles + 1.271 * doubles + 1.616 * triples + 2.101 * homeRuns) / (atBats + walks + sacrifices)).toString();
+    return ((.69 * walks + .888 * singles + 1.271 * doubles + 1.616 * triples + 2.101 * homeRuns) / (atBats + walks + sacrifices));
 }
 
-/*const test = [{
+
+/*test object to pass to mergeStats function
+
+const test = [{
  player: 'Mike',
  gameId: '',
- o: '',
- "1b": '',
- "2b": '',
- "3b": '',
- hr: '',
+ o: '180',
+ "1b": '170',
+ "2b": '80',
+ "3b": '20',
+ hr: '8',
  rbi: '',
  r: '',
- bb: '',
- k: '',
- sb: '',
- cs: '',
- sac: '',
+ bb: '20',
+ k: '0',
+ sb: '10',
+ cs: '2',
+ sac: '5',
 }];
-console.log('test', mergeStats([], test)[0].h);*/
+console.log('test', mergeStats([], test));*/
