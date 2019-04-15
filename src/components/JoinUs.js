30a82d3673fb7786a79d48947d44f577ba6cc033
vien.tang <vien.tang@veeva.com>
2019-04-15 06:31:49 -0700

    Join us to link to meetup page

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -5,7 +5,15 @@ import componentStyles from './components.module.css';
 const JoinUs = () => {
     return (
         <div className={componentStyles.joinUs}>
-            <h3 className={componentStyles.joinUsTitle}>Join us for the next game!</h3>
+            <h3 className={componentStyles.joinUsTitle}>
+                <a
+                    href="https://www.meetup.com/San-Francisco-Softball-Players/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Join us for the next game!
+                </a>
+            </h3>
         </div>
     );
 };
